91650577098fd95261ea447e21fb46197751e735
Dave Cooper <dave.cooper@checkout.com>
2016-10-31 18:08:46 +0000

    Update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const BUILD_DIR = path.resolve(__dirname, 'build');
 const APP_DIR = path.resolve(__dirname, 'src/app');
 
 const config = {
-  entry: `${APP_DIR}/components/CkoGrid.jsx`,
+  entry: `${APP_DIR}/client/App.jsx`,
   module: {
     loaders: [
-      { test: /\.jsx$/, exclude: /(node_modules|__tests__)/, loader: 'babel' },
+      { test: /\.jsx$/, exclude: /(node_modules|__tests__)/, loader: 'babel-loader', query: { presets: ['es2015'] } },
     ],
   },
   output: {
@@ -17,6 +18,10 @@ const config = {
   resolve: {
     extensions: ['', '.webpack.js', '.web.js', '.jsx', '.js'],
   },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'CKO Grid',
+    template: `${APP_DIR}/client/index.html`,
+  })],
   devtool: 'eval-cheap-module-source-map',
 };
 
