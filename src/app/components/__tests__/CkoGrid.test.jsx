4aba33b265a80cfccfff79c5fc9d710b061e0d47
Dave Cooper <dave.cooper@checkout.com>
2016-11-28 16:40:59 +0000

    Update CkoGrid tests

--- src/app/components/__tests__/CkoGrid.test.jsx
+++ src/app/components/__tests__/CkoGrid.test.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
+import sinon from 'sinon';
 import CkoGrid from '../CkoGrid';
 
+let refreshData;
+
 describe('CkoGrid', () => {
+  beforeEach(() => {
+    refreshData = sinon.stub();
+  });
+
   it('Renders', () => {
     const headings = {
       name: 'Name',
@@ -15,7 +21,14 @@ describe('CkoGrid', () => {
       { name: 'Dave', age: 26, favouriteFood: 'Peanut Butter' },
     ];
 
-    const component = shallow(<CkoGrid headings={headings} data={data} />);
+    const component = shallow(
+      <CkoGrid
+        headings={headings}
+        data={data}
+        refreshData={refreshData}
+        search={false}
+      />
+    );
 
     expect(component).toBeDefined();
   });
