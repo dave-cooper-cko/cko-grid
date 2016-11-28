80019eb74e793096649168c67dc43dd3343bdae5
Dave Cooper <dave.cooper@checkout.com>
2016-11-28 17:22:26 +0000

    Add TableDataRows tests

--- src/app/components/__tests__/TableDataRows.test.jsx
+++ src/app/components/__tests__/TableDataRows.test.jsx
@@ -13,19 +13,28 @@ describe('TableDataRows', () => {
     ];
 
     headings = {
-      name: 'Name',
-      age: 'Age',
-      favouriteFood: 'Favourite Food',
+      name: { name: 'Name', sort: true },
+      age: { name: 'Age', sort: true },
+      favouriteFood: { name: 'Favourite Food', sort: false },
     };
   });
 
-  it('Renders correctly', () => {
+  it('Renders successfully', () => {
     const component = shallow(<TableDataRows data={data} headings={headings} />);
 
     expect(component).toBeDefined();
   });
 
   it('Generates the right number of TableRows', () => {
-    const component = shallow(<TableDataRows data={data} headings={headings} />);
+    let component = shallow(<TableDataRows data={data} headings={headings} />);
+    let tableDataRows = component.find('TableDataRow');
+
+    expect(tableDataRows.length).toEqual(data.length);
+
+    data.push({ name: 'Vlad', age: 26, favouriteFood: 'Lettuce' });
+    component = shallow(<TableDataRows data={data} headings={headings} />);
+    tableDataRows = component.find('TableDataRow');
+
+    expect(tableDataRows.length).toEqual(data.length);
   });
 });
