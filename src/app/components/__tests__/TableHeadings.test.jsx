d4c979788b61cfeaea054d6d6f45d89575a460d7
Dave Cooper <dave.cooper@checkout.com>
2016-11-28 16:39:52 +0000

    Add TableHeadings tests

--- src/app/components/__tests__/TableHeadings.test.jsx
+++ src/app/components/__tests__/TableHeadings.test.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
+import sinon from 'sinon';
+import { spyOnComponentMethod } from 'sinon-spy-react';
 import TableHeadings from '../TableHeadings';
 
 let headings;
-
+let setRemoteState;
 
 describe('TableHeadings', () => {
   beforeEach(() => {
     headings = {
-      name: 'Name',
-      age: 'Age',
-      favouriteFood: 'Favourite Food',
+      name: { name: 'Name', sort: true },
+      age: { name: 'Age', sort: true },
+      favouriteFood: { name: 'Favourite Food', sort: false },
     };
+    setRemoteState = sinon.stub();
   });
 
   it('Renders successfully', () => {
-    const component = shallow(<TableHeadings headings={headings} />);
+    const component = shallow(
+      <TableHeadings
+        headings={headings}
+        setRemoteState={setRemoteState}
+      />
+    );
 
     expect(component).toBeDefined();
   });
@@ -24,17 +31,84 @@ describe('TableHeadings', () => {
   it('Renders the correct number of headings', () => {
     let numHeaders = Object.keys(headings).length;
 
-    let component = shallow(<TableHeadings headings={headings} />);
+    let component = shallow(
+      <TableHeadings
+        headings={headings}
+        setRemoteState={setRemoteState}
+      />
+    );
+
     let renderedHeaders = component.find('th');
 
     expect(numHeaders).toEqual(renderedHeaders.length);
 
     headings.newHeading = 'new';
 
-    numHeaders = Object.keys(headings).length;
-    component = shallow(<TableHeadings headings={headings} />);
+    numHeaders = 4;
+    component = shallow(
+      <TableHeadings
+        headings={headings}
+        setRemoteState={setRemoteState}
+      />
+    );
+
     renderedHeaders = component.find('th');
 
     expect(numHeaders).toEqual(renderedHeaders.length);
+  });
+
+  it('Sets the correct sortDirection when headers are clicked', () => {
+    const component = shallow(
+      <TableHeadings
+        headings={headings}
+        setRemoteState={setRemoteState}
+      />
+    );
+
+    let header = component.find('th').at(0);
+    header.simulate('click');
+    expect(component.state().sortDirection).toEqual('desc');
+    header.simulate('click');
+    expect(component.state().sortDirection).toEqual('asc');
+
+    header = component.find('th').at(1);
+    header.simulate('click');
+    expect(component.state().sortDirection).toEqual('desc');
+
+    header = component.find('th').at(0);
+    header.simulate('click');
+    expect(component.state().sortDirection).toEqual('desc');
+  });
+
+  it('Only renders a selector for sortable columns', () => {
+    const component = shallow(
+      <TableHeadings
+        headings={headings}
+        setRemoteState={setRemoteState}
+      />
+    );
+
+    let header = component.find('th').at(0);
+    expect(header.find('.selector').length).toEqual(1);
+
+    header = component.find('th').at(2);
+    expect(header.find('.selector').length).toEqual(0);
+  });
+
+  it('Does not call `toggleSort` for unsortable columns when the header is clicked', () => {
+    const component = shallow(
+      <TableHeadings
+        headings={headings}
+        setRemoteState={setRemoteState}
+      />
+    );
+
+    let header = component.find('th').at(0);
+    header.simulate('click');
+    const sortColumn = component.state().sortColumn;
+
+    header = component.find('th').at(2);
+    header.simulate('click');
+    expect(sortColumn).toEqual(component.state().sortColumn);
   });
 });
