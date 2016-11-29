6eaf369fc3155c5d43925a7cd74902890f74886d
Dave Cooper <dave.cooper@checkout.com>
2016-11-29 15:31:24 +0000

    Add Search tests

---
+++ src/app/components/__tests__/Search.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import Search from '../Search';
+
+const setRemoteState = sinon.stub();
+
+describe('Search', () => {
+  it('Renders successfully', () => {
+    const component = shallow(<Search setRemoteState={setRemoteState} />);
+
+    expect(component).toBeDefined();
+  });
+
+  it('Sets the correct searchText when text is entered', () => {
+    const component = shallow(<Search setRemoteState={setRemoteState} />);
+
+    expect(component.state().searchText).toEqual('');
+
+    const input = component.find('input');
+    input.simulate('change', { target: { value: 'dave' } });
+
+    expect(component.state().searchText).toEqual('dave');
+  });
+});
