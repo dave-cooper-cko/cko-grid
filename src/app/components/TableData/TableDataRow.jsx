093f749eed4a864fe69533044a727ae27420e183
Dave Cooper <dave.cooper@checkout.com>
2016-12-13 14:50:05 +0000

    Update TableDataRow to use ToggleExpand prop

--- src/app/components/TableData/TableDataRow.jsx
+++ src/app/components/TableData/TableDataRow.jsx
@@ -4,23 +4,53 @@ import styles from './styles.css';
 const propTypes = {
   headings: React.PropTypes.object.isRequired,
   data: React.PropTypes.object.isRequired,
+  toggleExpand: React.PropTypes.func,
 };
 
-function TableDataRow(props) {
-  const headings = Object.keys(props.headings);
+class TableDataRow extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <tr>
-      {headings.map(
-        (heading, index) =>
-          <td key={index}>
-            <div className={styles.container}>
-              {props.data[heading]}
+    this.state = {
+      expanded: false,
+    };
+
+    this.toggleExpand = this.toggleExpand.bind(this);
+  }
+
+  toggleExpand() {
+    this.setState({ expanded: !this.state.expanded });
+    this.props.toggleExpand();
+  }
+
+  render() {
+    const headings = Object.keys(this.props.headings);
+    const expanded = this.state.expanded ? styles.expanded : styles.expander;
+    const showExpander = (this.props.data.expand &&
+      Array.isArray(this.props.data.expand) &&
+      this.props.data.expand.length > 0) ||
+      (typeof this.props.data.expand === 'string');
+
+    return (
+      <tr>
+        <td>
+          { showExpander &&
+            <div onClick={this.toggleExpand} className={expanded}>
+              ▼
             </div>
-          </td>
-      )}
-    </tr>
-  );
+          }
+        </td>
+        {headings.map(
+          (heading, index) =>
+            <td key={index}>
+              <div className={styles.container}>
+                {this.props.data[heading]}
+              </div>
+            </td>
+        )}
+      </tr>
+    );
+  }
 }
 
 TableDataRow.propTypes = propTypes;
